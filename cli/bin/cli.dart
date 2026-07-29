d7d94bb711b5304701a57d4c9815e9326b7f4377
Krista Smith <kristaj@gmail.com>
2026-07-29 09:15:28 -0400

    initial set up

---
+++ cli/bin/cli.dart
@@ -0,0 +1,35 @@
+import 'dart:io';
+
+const version = '0.0.1';
+
+void main(List<String> arguments) {
+  if (arguments.isEmpty || arguments.first == 'help') {
+    printUsage();
+  } else if (arguments.first == 'version') {
+    print('Dartpedia CLI version $version');
+  } else if (arguments.first == 'search') {
+    final inputArgs = arguments.length > 1 ? arguments.sublist(1): null;
+    searchWikipedia(inputArgs);
+  } else {
+    printUsage();
+  }
+}
+
+void searchWikipedia(List<String>? arguments) {
+  final String articleTitle;
+  if (arguments == null || arguments.isEmpty) {
+    print('Please provide an article title');
+    articleTitle = stdin.readLineSync() ?? '';
+  } else {
+    articleTitle = arguments.join(' ');
+  }
+  print('Looking up articles about "$articleTitle". Please wait.');
+  print('Here ya go!');
+  print('(Pretend this is an article about "$articleTitle")');
+}
+
+void printUsage() {
+  print(
+    "The following commands are valid: 'help, 'version', 'search <ARTICLE-TITLE>'"
+  );
+}
