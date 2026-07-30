f4ced73849ef46aa33dec45199170c525739564e
Krista Smith <the.cozy.coder1@gmail.com>
2026-07-30 08:35:59 -0400

    Complete CLI application - bare bones

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'package:http/http.dart' as http;
 
 const version = '0.0.1';
 
@@ -7,7 +8,7 @@ void main(List<String> arguments) {
     printUsage();
   } else if (arguments.first == 'version') {
     print('Dartpedia CLI version $version');
-  } else if (arguments.first == 'search') {
+  } else if (arguments.first == 'wikipedia') {
     final inputArgs = arguments.length > 1 ? arguments.sublist(1): null;
     searchWikipedia(inputArgs);
   } else {
@@ -15,17 +16,23 @@ void main(List<String> arguments) {
   }
 }
 
-void searchWikipedia(List<String>? arguments) {
+void searchWikipedia(List<String>? arguments) async {
   final String articleTitle;
+
   if (arguments == null || arguments.isEmpty) {
     print('Please provide an article title');
-    articleTitle = stdin.readLineSync() ?? '';
+    final inputFromStdin = stdin.readLineSync() ?? '';
+    if (inputFromStdin.isEmpty) {
+      print("No article tite provided.  Exiting");
+      return;
+    }
+    articleTitle = inputFromStdin;  
   } else {
     articleTitle = arguments.join(' ');
   }
   print('Looking up articles about "$articleTitle". Please wait.');
-  print('Here ya go!');
-  print('(Pretend this is an article about "$articleTitle")');
+  var articleContent = await getWikipediaArticle(articleTitle);
+  print(articleContent);
 }
 
 void printUsage() {
@@ -33,3 +40,17 @@ void printUsage() {
     "The following commands are valid: 'help, 'version', 'search <ARTICLE-TITLE>'"
   );
 }
+
+Future<String> getWikipediaArticle(String articleTitle) async {
+  final url = Uri.https(
+    'en.wikipedia.org',
+    '/api/rest_v1/page/summary/$articleTitle',
+  );
+
+  final response = await http.get(url);
+  if (response.statusCode == 200) {
+    return response.body;
+  }
+  return 'Error: Failed to fetch article "$articleTitle". Status code ${response.statusCode}';
+}
+
