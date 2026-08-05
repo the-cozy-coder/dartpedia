b4dfa4e53bc52ba1c6a0a8438b37df1eae406f13
Krista Smith <the.cozy.coder1@gmail.com>
2026-08-05 08:08:37 -0400

    add command runner and argument parsing

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -1,12 +1,10 @@
-import 'dart:io';
-import 'package:http/http.dart' as http;
 import 'package:command_runner/command_runner.dart';
 
 const version = '0.0.1';
 
-void main(List<String> arguments) async {
-  var runner = CommandRunner();
-  await runner.run(arguments);
+void main(List<String> arguments){
+  var commandRunner = CommandRunner()..addCommand(HelpCommand());
+  commandRunner.run(arguments);
 }
 
 //   if (arguments.isEmpty || arguments.first == 'help') {
